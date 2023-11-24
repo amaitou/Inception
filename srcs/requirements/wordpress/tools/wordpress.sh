8eb233e9ea300eabfeac55798f2f58670ec28516
amait-ou <amait-ou@student.1337.ma>
2023-11-24 09:57:17 +0100

    check already existence

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
-#if which wp > /dev/null/ 2>1; then
-#	echo "[.] wp-cli is installed"
-#else
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-#fi
-
 mkdir -p /run/php/
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-if [ -e "/var/www/html/wordpress" ]; then
-	echo "[.] /var/www/wordpress already created.";
-else
-	mkdir -p /var/www/html/wordpress/;
+if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
+	exec "$@";
+	exit 0;
+fi
+
+if [ ! -d "/usr/local/bin/wp/" ]; then
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ ! -d "/var/www/html/wordpress" ]; then
+    mkdir -p /var/www/wordpress
 fi
 
 cd /var/www/html/wordpress
 rm -rf /var/www/html/wordpress/*
 
-#if ! wp core is-installed; then
 wp core download --allow-root
-#fi
 
 wp config create	--allow-root \
 					--dbname=$DATABASE_NAME \
 					--dbuser=$DATABASE_ROOT \
 					--dbpass=$DATABASE_ROOT_PASSWORD \
 					--dbhost=mariadb:3306 \
-					--path='/var/www/html/wordpress'
+					--path='/var/www/html/wordpress/'
 
 wp core install		--allow-root \
 					--url=$WORDPRESS_URL \
@@ -37,16 +36,10 @@ wp core install		--allow-root \
 					--admin_password=$WORDPRESS_ADMIN_PASSWORD \
 					--admin_email=$WORDPRESS_ADMIN_EMAIL
 
-#if wp user list --field=$DATABASE_SUB_USER --allow-root | grep -q "^$DATABASE_SUB_USER$"; then
-#	echo "[.] user already created"
-#else
 wp user create	--allow-root \
 				$WORDPRESS_SUB_USER \
 				$WORDPRESS_SUB_EMAIL \
 				--role=$WORDPRESS_SUB_ROLE \
 				--user_pass=$WORDPRESS_SUB_PASSWORD
-#fi
-
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 exec "$@"
