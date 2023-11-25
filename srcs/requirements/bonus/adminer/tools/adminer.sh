1d3d12699c50f686d22d661428178b4712c1cf1c
amait-ou <amait-ou@student.1337.ma>
2023-11-25 20:38:44 +0100

    added container monitoring - cadvisor

--- srcs/requirements/bonus/adminer/tools/adminer.sh
+++ srcs/requirements/bonus/adminer/tools/adminer.sh
@@ -7,6 +7,6 @@ mv adminer.php /var/www/html/adminer/
 
 cd /var/www/html/adminer/
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:8080/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:8000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 exec "$@"
